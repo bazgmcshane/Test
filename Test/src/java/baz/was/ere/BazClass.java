48760a91e1e836078a067be211cf67ed399ca563
Derek Fitzsimons <derek.fitzsimons@advancedcomputersoftware.com>
2015-11-24 11:25:18 +0000

    Adding a bunch of missing stuff

---
+++ Test/src/java/baz/was/ere/BazClass.java
@@ -0,0 +1,14 @@
+/*
+  =================================
+  (C) Derek Fitzsimons 2015
+  =================================
+ */
+package baz.was.ere;
+
+/**
+ *
+ * @author Derek.Fitzsimons
+ */
+public class BazClass {
+
+}
